e1e3d4f33032ec4d908bd7a29e3bcad8c95581a7
Dhairya Malhotra <dmalhotra@flatironinstitute.org>
2022-01-27 09:55:49 -0500

    Document ode-solver.hpp; and add src/test-ode-solver.cpp

--- include/sctl/ode-solver.hpp
+++ include/sctl/ode-solver.hpp
@@ -11,131 +11,104 @@ namespace SCTL_NAMESPACE {
 template <class ValueType> class Vector;
 template <class ValueType> class Matrix;
 
-template <class Real, Integer ORDER> class SDC {
+template <class Real> class SDC {
   public:
 
     using Fn = std::function<void(Vector<Real>*, const Vector<Real>&)>;
 
-    SDC() {
-      Vector<Real> x_cheb(ORDER);
-      for (Long i = 0; i < ORDER; i++) {
-        x_cheb[i] = 0.5 - 0.5 * cos(const_pi<Real>() * i / (ORDER - 1));
-      }
+    /**
+     * Constructor
+     *
+     * @param[in] Order the order of the method.
+     */
+    SDC(const Integer Order);
 
-      Matrix<Real> Mp(ORDER, ORDER);
-      Matrix<Real> Mi(ORDER, ORDER);
-      for (Long i = 0; i < ORDER; i++) {
-        for (Long j = 0; j < ORDER; j++) {
-          Mp[j][i] = pow<Real>(x_cheb[i],j);
-          Mi[j][i] = pow<Real>(x_cheb[i],j+1) / (j+1);
-        }
-      }
-      M_time_step = (Mp.pinv() * Mi).Transpose(); // TODO: replace Mp.pinv()
+    /**
+     * Apply one step of spectral deferred correction (SDC).
+     * Compute: u = \int_0^{dt} F(u)
+     *
+     * @param[out] u the solution
+     * @param[in] dt the step size
+     * @param[in] u0 the initial value
+     * @param[in] F the function du/dt
+     * @param[in] N_picard the maximum number of picard iterations
+     * @param[in] tol_picard the tolerance for stopping picard iterations
+     * @param[out] error_interp an estimate of the truncation error of the solution interpolant
+     * @param[out] error_picard the picard iteration error
+     */
+    void operator()(Vector<Real>* u, const Real dt, const Vector<Real>& u0, const Fn& F, Integer N_picard = -1, Real tol_picard = 0, Real* error_interp = nullptr, Real* error_picard = nullptr) const;
 
-      Mp.ReInit(ORDER,ORDER); Mp = 0;
-      Mi.ReInit(ORDER,ORDER); Mi = 0;
-      Integer TRUNC_ORDER = ORDER;
-      if (ORDER >= 2) TRUNC_ORDER = ORDER - 1;
-      if (ORDER >= 6) TRUNC_ORDER = ORDER - 1;
-      if (ORDER >= 9) TRUNC_ORDER = ORDER - 1;
-      for (Long j = 0; j < TRUNC_ORDER; j++) {
-        for (Long i = 0; i < ORDER; i++) {
-          Mp[j][i] = pow<Real>(x_cheb[i],j);
-          Mi[j][i] = pow<Real>(x_cheb[i],j);
-        }
-      }
-      M_error = (Mp.pinv() * Mi).Transpose(); // TODO: replace Mp.pinv()
-      for (Long i = 0; i < ORDER; i++) M_error[i][i] -= 1;
-    }
+    /**
+     * Solve ODE adaptively to required tolerance.
+     * Compute: u = \int_0^{T} F(u)
+     *
+     * @param[out] u the final solution
+     * @param[in] dt the initial step size guess
+     * @param[in] T the final time
+     * @param[in] u0 the initial value
+     * @param[in] F the function du/dt
+     * @param[in] tol_picard the required solution tolerance
+     *
+     * @return the final time (should equal T if no errors)
+     */
+    Real AdaptiveSolve(Vector<Real>* u, Real dt, const Real T, const Vector<Real>& u0, const Fn& F, Real tol) const;
 
-    // solve u = \int_0^{dt} F(u)
-    void operator()(Vector<Real>* u, const Real dt, const Vector<Real>& u0_, const Fn& F, Integer N_picard = ORDER, Real tol_picard = 0, Real* error_interp = nullptr, Real* error_picard = nullptr) {
-      auto max_norm = [] (const Matrix<Real>& M) {
-        Real max_val = 0;
-        for (Long i = 0; i < M.Dim(0); i++) {
-          for (Long j = 0; j < M.Dim(1); j++) {
-            max_val = std::max<Real>(max_val, fabs(M[i][j]));
-          }
-        }
-        return max_val;
-      };
-
-      const Long DOF = u0_.Dim();
-      Matrix<Real> Mu0(ORDER, DOF);
-      Matrix<Real> Mu1(ORDER, DOF);
-      for (Long j = 0; j < ORDER; j++) { // Set u0
-        for (Long k = 0; k < DOF; k++) {
-          Mu0[j][k] = u0_[k];
-        }
-      }
-
-      Matrix<Real> M_dudt(ORDER, DOF);
-      { // Set M_dudt
-        Vector<Real> dudt_(DOF, M_dudt[0], false);
-        F(&dudt_, Vector<Real>(DOF, Mu0[0], false));
-        for (Long i = 1; i < ORDER; i++) {
-          for (Long j = 0; j < DOF; j++) {
-            M_dudt[i][j] = M_dudt[0][j];
-          }
-        }
-      }
-      Mu1 = Mu0 + (M_time_step * M_dudt) * dt;
-
-      Matrix<Real> Merr(ORDER, DOF);
-      for (Long k = 0; k < N_picard; k++) { // Picard iteration
-        auto Mu_previous = Mu1;
-        for (Long i = 1; i < ORDER; i++) { // Set M_dudt
-          Vector<Real> dudt_(DOF, M_dudt[i], false);
-          F(&dudt_, Vector<Real>(DOF, Mu1[i], false));
-        }
-        Mu1 = Mu0 + (M_time_step * M_dudt) * dt;
-        Merr = Mu1 - Mu_previous;
-        if (max_norm(Merr) < tol_picard) break;
-      }
-
-      if (u->Dim() != DOF) u->ReInit(DOF);
-      for (Long k = 0; k < DOF; k++) { // Set u
-        u[0][k] = Mu1[ORDER - 1][k];
-      }
-      if (error_picard != nullptr) {
-        error_picard[0] = max_norm(Merr);
-      }
-      if (error_interp != nullptr) {
-        Merr = M_error * Mu1;
-        error_interp[0] = max_norm(Merr);
-      }
-    }
-
-    static void test() {
-      auto ref_sol = [](Real t) { return exp<Real>(-t); };
+    static void test_one_step(const Integer Order = 5) {
+      auto ref_sol = [](Real t) { return cos<Real>(-t); };
       auto fn = [](Vector<Real>* dudt, const Vector<Real>& u) {
-        dudt[0] = u * -1.0;
+        (*dudt)[0] = -u[1];
+        (*dudt)[1] = u[0];
       };
       std::function<void(Vector<Real>*, const Vector<Real>&)> F(fn);
 
-      SDC<Real, ORDER> ode_solver;
+      const SDC<Real> ode_solver(Order);
       Real t = 0.0, dt = 1.0e-1;
-      Vector<Real> u, u0(1);
-      u0 = 1.0;
+      Vector<Real> u, u0(2);
+      u0[0] = 1.0;
+      u0[1] = 0.0;
       while (t < 10.0) {
         Real error_interp, error_picard;
-        ode_solver(&u, dt, u0, F, ORDER, 0.0, &error_interp, &error_picard);
+        ode_solver(&u, dt, u0, F, -1, 0.0, &error_interp, &error_picard);
         { // Accept solution
           u0 = u;
           t = t + dt;
         }
 
         printf("t = %e;  ", t);
-        printf("u1 = %e;  ", u0[0]);
+        printf("u = %e;  ", u0[0]);
         printf("error = %e;  ", ref_sol(t) - u0[0]);
         printf("time_step_error_estimate = %e;  \n", std::max(error_interp, error_picard));
       }
     }
 
+    static void test_adaptive_solve(const Integer Order = 5, const Real tol = 1e-5) {
+      auto ref_sol = [](Real t) { return cos(-t); };
+      auto fn = [](Vector<Real>* dudt, const Vector<Real>& u) {
+        (*dudt)[0] = -u[1];
+        (*dudt)[1] = u[0];
+      };
+      std::function<void(Vector<Real>*, const Vector<Real>&)> F(fn);
+
+      Vector<Real> u, u0(2);
+      u0[0] = 1.0; u0[1] = 0.0;
+      Real T = 10.0, dt = 1.0e-1;
+
+      SDC<Real> ode_solver(Order);
+      Real t = ode_solver.AdaptiveSolve(&u, dt, T, u0, F, tol);
+
+      if (t == T) {
+        printf("u = %e;  ", u[0]);
+        printf("error = %e;  \n", ref_sol(T) - u[0]);
+      }
+    }
+
   private:
     Matrix<Real> M_time_step, M_error;
+    Integer Order;
 };
 
 }
+
+#include SCTL_INCLUDE(ode-solver.txx)
 
 #endif  //_SCTL_ODE_SOLVER_
