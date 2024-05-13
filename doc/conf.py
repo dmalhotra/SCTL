38d80aa7f3e1005c0f63c7d5b4452cb474e4d38d
Dhairya Malhotra <dhairya.malhotra@gmail.com>
2024-05-13 18:14:21 -0400

    Add Read the Docs

--- doc/conf.py
+++ doc/conf.py
@@ -80,6 +80,7 @@ subprocess.call('doxygen', shell=True)
 
 # -- Options for HTML output -------------------------------------------------
 
+html_theme = 'sphinx_rtd_theme'
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
 
@@ -87,7 +88,6 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     # a list of builtin themes.
     #
     # html_theme = 'alabaster'
-    html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
     # Theme options are theme-specific and customize the look and feel of a theme
